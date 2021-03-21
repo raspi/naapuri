741df3cc1da07645a48ee52c5c7c64cdafe3f05a
raspi <pekka.jarvinen@gmail.com>
2021-03-21 08:16:09 +0200

    Usage and version

--- main.go
+++ main.go
@@ -2,18 +2,68 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"github.com/raspi/naapuri/pkg/neighbor"
 	"github.com/raspi/naapuri/pkg/neighbor/parser"
 	"os"
 	"os/signal"
+	"strings"
 )
 
 func OnEvent(evt parser.ChangeEvent) {
 	fmt.Println(evt)
 }
 
+var (
+	// These are set with Makefile -X=main.VERSION, etc
+	VERSION   = `v0.0.0`
+	BUILD     = `dev`
+	BUILDDATE = `0000-00-00T00:00:00+00:00`
+)
+
+const (
+	AUTHOR   = `Pekka Järvinen`
+	YEAR     = 2021
+	HOMEPAGE = `https://github.com/raspi/naapuri`
+)
+
 func main() {
+	showVersionArg := flag.Bool(`V`, false, `Show version`)
+
+	flag.Usage = func() {
+		_, _ = fmt.Fprintf(os.Stdout, `naapuri - show network mac address changes`+"\n")
+		_, _ = fmt.Fprintf(os.Stdout, `Version %v (%v)`+"\n", VERSION, BUILDDATE)
+		_, _ = fmt.Fprintf(os.Stdout, `(c) %v %v- [ %v ]`+"\n", AUTHOR, YEAR, HOMEPAGE)
+		_, _ = fmt.Fprintf(os.Stdout, "\n")
+
+		_, _ = fmt.Fprintf(os.Stdout, "Parameters:\n")
+
+		paramMaxLen := 0
+
+		flag.VisitAll(func(f *flag.Flag) {
+			l := len(f.Name)
+			if l > paramMaxLen {
+				paramMaxLen = l
+			}
+		})
+
+		flag.VisitAll(func(f *flag.Flag) {
+			padding := strings.Repeat(` `, paramMaxLen-len(f.Name))
+			_, _ = fmt.Fprintf(os.Stdout, "  -%s%s   %s   default: %q\n", f.Name, padding, f.Usage, f.DefValue)
+		})
+
+		_, _ = fmt.Fprintf(os.Stdout, "\n")
+	}
+
+	flag.Parse()
+
+	if *showVersionArg {
+		// Show version information
+		_, _ = fmt.Fprintf(os.Stdout, `Version %s build %s built on %s`+"\n", VERSION, BUILD, BUILDDATE)
+		os.Exit(0)
+	}
+
 	ctx := context.Background()
 	ctx, cancel := context.WithCancel(ctx)
 	signalChan := make(chan os.Signal, 1)
